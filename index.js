9ca1cdad554d0edf7b7afa2bf303cab9189d300f
tamotsu ichiba <ichibat@gmail.com>
2020-01-25 16:30:43 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+//initial requirement
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+//mongoDB options
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+//port setting
+const port = process.env.PORT || 3000;
+// app for express
+const app = express();
+
+//Use following middlewares
+//Helmet
+app.use(helmet());
+//express.json()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+//Cor
+app.use(cors());
+
+//Hello World!
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+ });
+
+ // port listening
+app.listen(port, () => console.log(`OK, Server started on port ${port}`));
